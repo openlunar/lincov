Corrected measurement covariance frame transform.

lincov/__init__.py
@@ -130,7 +130,7 @@ class LinCov(object):
             
         H = np.zeros((3, self.N))
         H[0:3,0:3] = x.T_body_to_cam.dot(x.T_inrtl_to_body)
-        H[0:3,6:9] = -x.T_body_to_cam.dot(pq.skew(x.T_inrtl_to_body.dot(r_pci)))
+        H[0:3,6:9] = x.T_body_to_cam.dot(pq.skew(x.T_inrtl_to_body.dot(r_pci)))
 
         return H, R_cam
 

lincov/horizon.py
@@ -21,6 +21,11 @@ def compute_T_pa_to_cam(time, body_id = 301, spacecraft_id = -5440):
     (arbitrarily). Thus, theta = 0 is in the center of the illuminated
     horizon arc.
 
+    Note that this is not the same principal axis frame defined in the
+    other horizon papers! This one is a cone principal axis frame,
+    used for the covariance model; the other is the planet's principal
+    axis-aligned frame, used in the measurement formulation.
+
     Args:
       time           ephemeris time (s)
       body_id        NAIF body identifier (defaults to 301, moon)
@@ -96,7 +101,7 @@ def covariance(time,
                   [0.0, 4 / (2 * theta_max - np.sin(2*theta_max)), 0.0],
                   [corner, 0.0, (rho**2 - r_p**2) * (2 * theta_max + np.sin(2*theta_max)) / (4 * D * r_p**2)]])
     scalar = sigma_pix**2 * rho**4 * theta_max / (n * d_x**2 * (rho**2 - r_p**2))
-    P = T_p_to_c.T.dot(M).dot(T_p_to_c) * scalar
+    P = T_p_to_c.dot(M).dot(T_p_to_c.T) * scalar
 
     if statistics:
         r_sun_wrt_body = spice.spkezp(body_id, time, 'J2000', 'LT', 10)[0]